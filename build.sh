ef3cb2896b2d11e486f936562fa79d333b2079ff
Don <don.j.olmstead@gmail.com>
2015-09-06 10:19:10 -0700

    Fixing clone in build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 git clone https://github.com/dogma-dart/dogma-codegen.git ../dogma-codegen
-git clone https://github.com/dogma-dart/dogma-data.git -b features/simplify-serialization ../dogma-data
+git clone https://github.com/dogma-dart/dogma-data.git ../dogma-data
 git clone https://github.com/dogma-dart/dogma-json-schema.git ../dogma-json-schema
+
+dart --version
 
 pub install
 
